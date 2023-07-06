c276245b4723bb3027c40d3ae13db3c994825ac8
MagriniLorenzo <138790523+MagriniLorenzo@users.noreply.github.com>
2023-07-06 15:47:25 +0200

    Aggiunta Metodo Verifica Admin
    Aggiunta Metodo Verifica Admin

--- src/basic/Amministratore.java
+++ src/basic/Amministratore.java
@@ -1,11 +1,39 @@
 package basic;
 
 import java.util.ArrayList;
+import java.util.*;
+import java.io.*;
 
 public class Amministratore extends Giocatore{
+	
 	String password;
+	
 	public Amministratore(String nome, String password, int nVite, ArrayList<Carta> carte, long punteggio) {
 		super(nome, nVite, carte, punteggio);
 		this.password=password;
+	}
+	
+	public boolean VerificaAdmin(String username, String password) {
+		
+		String u;
+		String p;
+		
+		File f = new File("Passwords.txt");
+		
+		Scanner scan = new Scanner(f);
+		
+		while(scan.hasNextLine()) {
+			u = scan.nextString();
+			p = scan.nextString();
+			
+			if(u == username && p == password)
+				{
+					return true;
+				}else
+				{
+					return false;
+				}
+			}
+		}
 	}
 }
