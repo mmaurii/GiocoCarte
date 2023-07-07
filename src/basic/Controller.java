c39e7463a239f9d602ed70897c77e76edcb99dd8
MagriniLorenzo <138790523+MagriniLorenzo@users.noreply.github.com>
2023-07-07 15:01:29 +0200

    Controller

--- src/basic/Controller.java
+++ src/basic/Controller.java
@@ -1,31 +1,29 @@
 package basic;
-import java.awt.TextField;
-import java.util.Scanner;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class Controller {
-	@FXML TextField username;
-	@FXML TextField password;
-	@FXML Label lblVerifica;
+    @FXML
+    private TextField txtUsername;
 
-	public void contolloLogin(ActionEvent ae) {
-		Amministratore a;
-		boolean flag = false;
-		do {
-			String p = password.getText();
-			//String u = username.getText();
-			System.out.println(p);
-			
+    @FXML
+    private TextField txtPassword;
 
-			a = new Amministratore(p, p);
-			flag = a.verificaAdmin();
-		}while(!flag);
-		
-		if(a!=null) {
-			lblVerifica.setText("verificato");
-		}
-	}
+    String u = "admin";
+    String p = "admin";
+
+    @FXML
+    public void loginAction(ActionEvent actionEvent) {
+
+        String username_text = txtUsername.getText();
+        String password_text = txtPassword.getText();
+
+        if(username_text.compareTo(u) == 0 && password_text.compareTo(p) == 0)
+        {
+            txtUsername.setText("Loggato");
+        }
+    }
 }
-
